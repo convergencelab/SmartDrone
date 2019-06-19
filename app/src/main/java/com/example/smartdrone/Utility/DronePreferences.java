ee9a3334a29eb45981f9e1f1687b750895fa8aec
travismacdonald <x2015khn@stfx.ca>
2019-06-19 15:56:33 -0300

    Added Shared Preferences helper class.

---
+++ app/src/main/java/com/example/smartdrone/Utility/DronePreferences.java
@@ -0,0 +1,32 @@
+package com.example.smartdrone.Utility;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class DronePreferences {
+
+    private static final String BASS_NOTE_KEY = "bassNoteEnabled"; //todo extract to string resource
+
+    private static SharedPreferences getSharedPrefs(Context context) {
+        return android.support.v7.preference.PreferenceManager.getDefaultSharedPreferences(context);
+    }
+//    private static SharedPreferences sharedPrefs = android.support.v7.preference.PreferenceManager.getDefaultSharedPreferences();
+
+    /**
+     * Get boolean for bass switch.
+     * @param       context Context; context.
+     * @return      boolean; data for bass switch.
+     */
+    public static boolean getStoredBassPref(Context context) {
+        return getSharedPrefs(context).getBoolean(BASS_NOTE_KEY, true);
+    }
+
+    /**
+     * Set boolean for bass switch.
+     * @param       context Context, context.
+     * @param       bool boolean; bass switch data.
+     */
+    public static void setStoredBassPref(Context context, boolean bool) {
+        getSharedPrefs(context).edit().putBoolean(BASS_NOTE_KEY, bool).apply();
+    }
+}
