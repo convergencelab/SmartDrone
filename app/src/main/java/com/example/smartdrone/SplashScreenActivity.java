680847dd59c5f6a9dce17fb03cf520ae0b7e108b
travismacdonald <x2015khn@stfx.ca>
2019-06-24 08:47:01 -0300

    Splash Screen Aspect Ratio Fixed

--- app/src/main/java/com/example/smartdrone/SplashScreenActivity.java
+++ app/src/main/java/com/example/smartdrone/SplashScreenActivity.java
@@ -4,18 +4,28 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.example.smartdrone.Utility.DroneLog;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    /**
+     * Duration of splash screen in milliseconds.
+     */
+    private static final int SPLASH_DURATION = 1100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        DroneLog.activityLifecycle("Splash Screen Activity", "onCreate");
-        
+        setContentView(R.layout.activity_splash_screen);
 
-        Intent intent = new Intent(this, DroneActivity.class);
-        startActivity(intent);
-        finish();
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), DroneActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DURATION);
     }
 }
