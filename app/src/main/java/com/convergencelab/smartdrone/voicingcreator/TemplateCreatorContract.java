b001a68f95d069bd0c54c09912e12f1de291f3ce
travismacdonald <x2015khn@stfx.ca>
2019-07-15 11:14:07 -0300

    Added methods in VoicingCreatorContract

--- app/src/main/java/com/convergencelab/smartdrone/voicingcreator/TemplateCreatorContract.java
+++ app/src/main/java/com/convergencelab/smartdrone/voicingcreator/TemplateCreatorContract.java
@@ -2,14 +2,27 @@ package com.convergencelab.smartdrone.voicingcreator;
 
 import com.convergencelab.smartdrone.BasePresenter;
 import com.convergencelab.smartdrone.BaseView;
+import com.example.keyfinder.Note;
 
 public interface TemplateCreatorContract {
 
     interface View extends BaseView<Presenter> {
 
+        void showNoteActive(Note toShow);
+
+        void showNoteInactive(Note toShow);
+
     }
-    
+
     interface Presenter extends BasePresenter {
+
+        void start();
+
+        void toggleNoteStatus(Note toToggle);
+
+        void saveTemplate();
+
+        void cancel();
 
     }
 
