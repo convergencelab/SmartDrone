7b9f552e597503dd12301c88c8284fa1b15521d6
travismacdonald <x2015khn@stfx.ca>
2020-06-17 11:01:41 -0300

    Added piano key resourse files

---
+++ app/src/main/java/com/convergencelabstfx/smartdrone/v2/views/PianoView.java
@@ -0,0 +1,23 @@
+package com.convergencelabstfx.smartdrone.v2.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+class PianoView extends ConstraintLayout {
+
+    public PianoView(Context context) {
+        super(context);
+    }
+
+    public PianoView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PianoView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+}
