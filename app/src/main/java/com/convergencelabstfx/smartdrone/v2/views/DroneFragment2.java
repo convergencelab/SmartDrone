0e6e1b192a2920f64020186cc416dcb4aecbfb04
travismacdonald <x2015khn@stfx.ca>
2020-06-17 14:20:41 -0300

    Added piano key click functionality

--- app/src/main/java/com/convergencelabstfx/smartdrone/v2/views/DroneFragment2.java
+++ app/src/main/java/com/convergencelabstfx/smartdrone/v2/views/DroneFragment2.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.convergencelabstfx.smartdrone.R;
 import com.convergencelabstfx.smartdrone.databinding.FragmentDrone2Binding;
@@ -16,6 +19,8 @@ import org.jetbrains.annotations.NotNull;
 
 public class DroneFragment2 extends Fragment {
 
+    private FragmentDrone2Binding binding;
+
     public DroneFragment2() {
         // Required empty public constructor
     }
@@ -23,11 +28,19 @@ public class DroneFragment2 extends Fragment {
     @Override
     public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        FragmentDrone2Binding binding = DataBindingUtil.inflate(
+        binding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_drone_2, container, false
         );
         PianoView piano = new PianoView(getContext());
+        for (int i = 0; i < 12; i++) {
+            final int ix = i;
+            piano.getPianoKeyAt(ix).setOnClickListener(view -> pianoClick(ix));
+        }
         binding.container.addView(piano);
         return binding.getRoot();
+    }
+
+    private void pianoClick(int key) {
+        Log.d("test", "" + key);
     }
 }
--- app/src/main/java/com/convergencelabstfx/smartdrone/v2/views/PianoView.java
+++ app/src/main/java/com/convergencelabstfx/smartdrone/v2/views/PianoView.java
@@ -3,12 +3,20 @@ package com.convergencelabstfx.smartdrone.v2.views;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.convergencelabstfx.smartdrone.R;
 
+import java.util.HashMap;
+
 class PianoView extends ConstraintLayout {
+
+    private HashMap<Integer, ImageButton> pianoKeyMap;
+    private LinearLayout whiteKeys;
+    private LinearLayout blackKeys;
 
     public PianoView(Context context) {
         super(context);
@@ -25,10 +33,40 @@ class PianoView extends ConstraintLayout {
         init(context);
     }
 
+    public ImageButton getPianoKeyAt(int ix) {
+        return pianoKeyMap.get(ix);
+    }
+
     private void init(Context context) {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         inflater.inflate(R.layout.piano_view, this);
+        whiteKeys = findViewById(R.id.whiteKeys);
+        blackKeys = findViewById(R.id.blackKeys);
+        setupPianoLocationMap();
     }
 
+    private void setupPianoLocationMap() {
+        // If you look at 'piano_view.xml', the keys are contained in 2 linear layouts;
+        // one for the white keys, and the other for the black keys.
+        // Because of this, the coordinates of the piano keys are not sequential
+        // and actually have a sort of staggered ordering.
+        //
+        //     ACTUAL |   (   1   3       6   8   10   ) -> black keys
+        //     ORDER  |   ( 0   2   4   5   7   9   11 ) -> white keys
+        //
+        // For this reason, the locations are mapped so that the index of the pitch class
+        // map will give you back the corresponding key button.
+        final int[] seqBlack = { 1, 3, 6, 8, 10 };
+        final int[] seqWhite = { 0, 2, 4, 5, 7, 9, 11 };
+        pianoKeyMap = new HashMap<>(seqWhite.length + seqBlack.length);
+        for (int i = 0; i < seqBlack.length; i++) {
+            ImageButton pianoKey = (ImageButton) blackKeys.getChildAt(i);
+            pianoKeyMap.put(seqBlack[i], pianoKey);
+        }
+        for (int i = 0; i < seqWhite.length; i++) {
+            ImageButton pianoKey = (ImageButton) whiteKeys.getChildAt(i);
+            pianoKeyMap.put(seqWhite[i], pianoKey);
+        }
+    }
 
 }
