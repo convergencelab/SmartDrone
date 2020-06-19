63db0f681ada4f4c3a2dd868df83501576510553
travismacdonald <x2015khn@stfx.ca>
2020-06-18 21:28:43 -0300

    All components ready for custom piano drawing; needs refactoring

--- app/src/main/java/com/convergencelabstfx/smartdrone/v2/views/PianoView.java
+++ app/src/main/java/com/convergencelabstfx/smartdrone/v2/views/PianoView.java
@@ -1,8 +1,14 @@
 package com.convergencelabstfx.smartdrone.v2.views;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
@@ -10,6 +16,7 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.convergencelabstfx.smartdrone.R;
 import com.example.keyfinder.MusicTheory;
+
 
 import java.util.HashMap;
 
@@ -19,6 +26,8 @@ public class PianoView extends ConstraintLayout {
     private LinearLayout blackKeys;
 
     private HashMap<Integer, ImageButton> pianoKeyMap = new HashMap<>(MusicTheory.TOTAL_NOTES);
+
+    final float scale = getContext().getResources().getDisplayMetrics().density;
 
     private boolean[] keyIsPressed = new boolean[12];
 
@@ -34,7 +43,22 @@ public class PianoView extends ConstraintLayout {
 
     public PianoView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init(context);
+        TypedArray a = context.getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.PianoView,
+                0, 0
+        );
+        Log.d("test", "H: " + attrs.getAttributeValue("http://schemas.android.com/apk/res/android", "layout_height"));
+        Log.d("test", "W: " + attrs.getAttributeValue("http://schemas.android.com/apk/res/android", "layout_width"));
+//        final int whiteKeyWidth = a.getInteger(R.styleable.Pia)
+        this.addView(makePianoKeyButton(
+                context,
+                100,
+                100,
+                2,
+                2,
+                Color.BLUE));
+        setupPianoLocationMap();
     }
 
     public PianoView(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -60,8 +84,7 @@ public class PianoView extends ConstraintLayout {
             // Show regular color
             if (isWhiteKey[ix]) {
                 getPianoKeyAt(ix).setBackgroundResource(R.drawable.piano_key_background_white);
-            }
-            else {
+            } else {
                 getPianoKeyAt(ix).setBackgroundResource(R.drawable.piano_key_background_black);
             }
         }
@@ -90,17 +113,44 @@ public class PianoView extends ConstraintLayout {
         //
         // For this reason, the locations are mapped so that the index of the pitch class
         // map will give you back the corresponding key button.
-        final int[] seqBlack = { 1, 3, 6, 8, 10 };
-        final int[] seqWhite = { 0, 2, 4, 5, 7, 9, 11 };
+//        final int[] seqBlack = {1, 3, 6, 8, 10};
+//        final int[] seqWhite = {0, 2, 4, 5, 7, 9, 11};
+//
+//        for (int i = 0; i < seqBlack.length; i++) {
+//            ImageButton pianoKey = (ImageButton) blackKeys.getChildAt(i);
+//            pianoKeyMap.put(seqBlack[i], pianoKey);
+//        }
+//        for (int i = 0; i < seqWhite.length; i++) {
+//            ImageButton pianoKey = (ImageButton) whiteKeys.getChildAt(i);
+//            pianoKeyMap.put(seqWhite[i], pianoKey);
+//        }
+    }
 
-        for (int i = 0; i < seqBlack.length; i++) {
-            ImageButton pianoKey = (ImageButton) blackKeys.getChildAt(i);
-            pianoKeyMap.put(seqBlack[i], pianoKey);
-        }
-        for (int i = 0; i < seqWhite.length; i++) {
-            ImageButton pianoKey = (ImageButton) whiteKeys.getChildAt(i);
-            pianoKeyMap.put(seqWhite[i], pianoKey);
-        }
+    private View makePianoKeyButton(
+            Context context,
+            int width,
+            int height,
+            int strokeWidth,
+            int cornerRadius,
+            int fillColor) {
+
+        ImageButton pianoKeyButton = new ImageButton(context);
+
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(dpsToPx(width), dpsToPx(height));
+        pianoKeyButton.setLayoutParams(params);
+
+        GradientDrawable drawable = new GradientDrawable();
+        drawable.setShape(GradientDrawable.RECTANGLE);
+        drawable.setColor(Color.GRAY);
+        drawable.setStroke(dpsToPx(strokeWidth), Color.BLACK);
+        drawable.setCornerRadius(dpsToPx(cornerRadius));
+        pianoKeyButton.setBackground(drawable);
+
+        return pianoKeyButton;
+    }
+
+    private int dpsToPx(float dps) {
+        return (int) (dps * scale + 0.5f);
     }
 
 }
