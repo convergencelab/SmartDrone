f85110def97eaaeb04e61af3d3b109107f568af7
travismacdonald <x2015khn@stfx.ca>
2020-07-16 10:00:22 -0300

    added PianoView dependency to project

--- app/src/main/java/com/convergencelabstfx/smartdrone/v2/views/DroneFragment2.java
+++ app/src/main/java/com/convergencelabstfx/smartdrone/v2/views/DroneFragment2.java
@@ -10,6 +10,8 @@ import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.convergencelabstfx.pianoview.PianoTouchListener;
+import com.convergencelabstfx.pianoview.PianoView;
 import com.convergencelabstfx.smartdrone.R;
 import com.convergencelabstfx.smartdrone.databinding.FragmentDrone2Binding;
 import com.convergencelabstfx.smartdrone.v2.viewmodels.DroneViewModel;
@@ -18,7 +20,7 @@ import org.jetbrains.annotations.NotNull;
 
 public class DroneFragment2 extends Fragment {
 
-    private FragmentDrone2Binding binding;
+    private FragmentDrone2Binding mBinding;
 
     private DroneViewModel mViewModel;
 
@@ -31,60 +33,12 @@ public class DroneFragment2 extends Fragment {
     @Override
     public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        binding = DataBindingUtil.inflate(
+        mBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_drone_2, container, false
         );
-//        addPiano();
-        binding.piano.addPianoTouchListener(new PianoTouchListener() {
-            @Override
-            public void onPianoTouch(PianoView piano, int key) {
-//                Log.d("testV", "" + key);
-                // Piano key change
-                if (key != lastKeyPressed) {
-                    if (lastKeyPressed != -1) {
-                        piano.showKeyNotPressed(lastKeyPressed);
-                    }
-                    if (key != -1) {
-                        piano.showKeyPressed(key);
-                    }
-                    lastKeyPressed = key;
-                }
-
-            }
-
-            @Override
-            public void onPianoClick(PianoView piano, int key) {
-//                if (piano.keyIsPressed(key)) {
-//                    piano.showKeyNotPressed(key);
-//                }
-//                else {
-//                    piano.showKeyPressed(key);
-//                }
-            }
-        });
         mViewModel = new ViewModelProvider(requireActivity()).get(DroneViewModel.class);
         Log.d("testV", mViewModel.testField);
-        return binding.getRoot();
+        return mBinding.getRoot();
     }
-
-    private void pianoClick(int key) {
-        Log.d("test", "" + key);
-    }
-//
-//    private void addPiano() {
-//        PianoView piano = new PianoView(getContext());
-//        for (int i = 0; i < 12; i++) {
-//            final int ix = i;
-//            piano.getPianoKeyAt(ix).setOnClickListener(view -> {
-//                if (!piano.keyIsPressed(ix)) {
-//                    piano.showKeyPressed(ix);
-//                } else {
-//                    piano.showKeyNotPressed(ix);
-//                }
-//                pianoClick(ix);
-//            });
-//        }
-//        binding.container.addView(piano);
-//    }
 
 }
--- app/src/main/java/com/convergencelabstfx/smartdrone/v2/views/PianoKey.java
+++
@@ -1,6 +0,0 @@
-package com.convergencelabstfx.smartdrone.v2.views;
-
-// todo: make wrapper class
-//       store primary key color, active key color, ...
-class PianoKey {
-}
