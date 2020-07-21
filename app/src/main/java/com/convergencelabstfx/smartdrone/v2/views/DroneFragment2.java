08f4d55fcb883d9ca635bb8eb0daeefb38acf004
travismacdonald <x2015khn@stfx.ca>
2020-07-21 10:16:19 -0300

    added miscrophone runtime permission

--- app/src/main/java/com/convergencelabstfx/smartdrone/v2/views/DroneFragment2.java
+++ app/src/main/java/com/convergencelabstfx/smartdrone/v2/views/DroneFragment2.java
@@ -1,5 +1,7 @@
 package com.convergencelabstfx.smartdrone.v2.views;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -7,6 +9,9 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
@@ -18,6 +23,8 @@ import com.convergencelabstfx.smartdrone.databinding.FragmentDrone2Binding;
 import com.convergencelabstfx.smartdrone.v2.viewmodels.DroneViewModel;
 
 public class DroneFragment2 extends Fragment {
+
+    private int MICROPHONE_PERMISSION_CODE = 1;
 
     private FragmentDrone2Binding mBinding;
 
@@ -37,6 +44,10 @@ public class DroneFragment2 extends Fragment {
         );
         mViewModel = new ViewModelProvider(requireActivity()).get(DroneViewModel.class);
 
+        if (!hasMicrophoneRuntimePermission()) {
+            requestMicrophonePermission();
+        }
+
         mViewModel.getSignalProcessor().addPitchListener(pitch -> {
                     Log.d("testV", Integer.toString(pitch));
                     if (pitch != mLastKey) {
@@ -50,6 +61,8 @@ public class DroneFragment2 extends Fragment {
                     }
                 }
         );
+
+
 
         mBinding.piano.addPianoTouchListener(new PianoTouchListener() {
             @Override
@@ -71,6 +84,24 @@ public class DroneFragment2 extends Fragment {
         return mBinding.getRoot();
     }
 
+    private boolean hasMicrophoneRuntimePermission() {
+        return ContextCompat.checkSelfPermission(
+                mBinding.getRoot().getContext(), Manifest.permission.RECORD_AUDIO)
+                == PackageManager.PERMISSION_GRANTED;
+    }
 
+    private void requestMicrophonePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.RECORD_AUDIO)) {
+            new AlertDialog.Builder(getContext())
+                    .setTitle("Permission Needed")
+                    .setMessage("This permission is needed to process pitch.")
+                    .setPositiveButton("ok", (dialog, which) -> ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.RECORD_AUDIO}, MICROPHONE_PERMISSION_CODE))
+                    .setNegativeButton("cancel", (dialog, which) -> dialog.dismiss())
+                    .create().show();
+        }
+        else {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.RECORD_AUDIO}, MICROPHONE_PERMISSION_CODE);
+        }
+    }
 
 }
