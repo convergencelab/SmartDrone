9b018d6f6eae0f871c8a17e0f5c2eef5bc9fc3c0
travismacdonald <x2015khn@stfx.ca>
2020-07-26 14:41:38 -0300

    implemented some overhead

---
+++ app/src/main/java/com/convergencelabstfx/smartdrone/DroneSettingsItem.java
@@ -0,0 +1,21 @@
+package com.convergencelabstfx.smartdrone;
+
+public interface DroneSettingsItem {
+
+    class ListItem {
+
+        public ListItem() {
+
+        }
+
+    }
+
+    class CheckBoxItem {
+
+        public CheckBoxItem() {
+
+        }
+
+    }
+
+}
--- app/src/main/java/com/convergencelabstfx/smartdrone/views/DroneSettingsFragment.java
+++ app/src/main/java/com/convergencelabstfx/smartdrone/views/DroneSettingsFragment.java
@@ -6,17 +6,26 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.convergencelabstfx.smartdrone.R;
-
 import com.convergencelabstfx.smartdrone.databinding.FragmentDroneSettingsBinding;
 import com.convergencelabstfx.smartdrone.viewmodels.DroneViewModel;
 
+/**
+ * Settings:
+ *     .) KeyDetector (activeNoteList, phraseDetector)
+ *         - List
+ *     .) if (activeNoteList) -> Mode
+ *         - List (parent scale) -> List (mode)
+ *     .) Change Keys on Piano Touch
+ *         - checkbox
+ *     .) Template List
+ *         - List; last option takes to template creator fragment
+ *
+ */
 public class DroneSettingsFragment extends Fragment {
 
     private DroneViewModel mViewModel;
@@ -35,15 +44,6 @@ public class DroneSettingsFragment extends Fragment {
                 inflater, R.layout.fragment_drone_settings, container, false
         );
         mViewModel = new ViewModelProvider(requireActivity()).get(DroneViewModel.class);
-
-        // todo: remove, just a reference on how to use livedata
-        final Observer<String> nameObserver = new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable final String newText) {
-                // Update the UI, in this case, a TextView.
-                mBinding.textView.setText(newText);
-            }
-        };
 
         return mBinding.getRoot();
     }
