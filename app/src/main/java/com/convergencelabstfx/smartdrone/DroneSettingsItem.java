8016202a71f871c904deb20f05d79e6b76cc64ec
travismacdonald <x2015khn@stfx.ca>
2020-07-26 16:51:39 -0300

    added layout res for list item for drone settings

--- app/src/main/java/com/convergencelabstfx/smartdrone/DroneSettingsItem.java
+++ app/src/main/java/com/convergencelabstfx/smartdrone/DroneSettingsItem.java
@@ -4,8 +4,26 @@ public interface DroneSettingsItem {
 
     class ListItem {
 
-        public ListItem() {
+        private String mTitle;
+        private String mSummary;
+        private int mIcon;
 
+        public ListItem(String title, String summary, int icon) {
+            mTitle = title;
+            mSummary = summary;
+            mIcon = icon;
+        }
+
+        public String getTitle() {
+            return mTitle;
+        }
+
+        public String getSummary() {
+            return mSummary;
+        }
+
+        public int getIcon() {
+            return mIcon;
         }
 
     }
